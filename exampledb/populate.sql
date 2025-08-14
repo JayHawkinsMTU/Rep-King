11e5e21cf79d613ac795524f520e90b10af2ad82
Jay <74466030+JayHawkinsMTU@users.noreply.github.com>
2025-08-14 15:34:00 -0400

    feat: populate example db and added queries

---
+++ exampledb/populate.sql
@@ -0,0 +1,27 @@
+PRAGMA foreign_keys = ON;
+
+INSERT INTO users(user_id)
+VALUES ("dev0-jay");
+
+INSERT INTO user_names (user_name_id, prior_user_name_id, user_id, name)
+VALUES ("ignore", null, "dev0-jay", "not-final-name"),
+    ("final", "ignore", "dev0-jay", "Jay");
+    
+INSERT INTO muscle_groups (muscle_group_name)
+VALUES ("biceps"), ("calves"), ("chest"), ("core"), ("forearms"), ("glutes"),
+    ("hamstrings"), ("hips"), ("lats"), ("lower-back"), ("shoulders"), ("quads"),
+    ("traps"), ("triceps");
+    
+-- Use this timestamp for everything: 2025-08-09 00:00:00.000
+-- (YYYY-MM-DD HH:MM:SS.SSS)
+INSERT INTO exercises(exercise_name, timestamp, created_by_user_id, is_isometric)
+VALUES ("bench-press", "2025-08-09 00:00:00.000", "dev0-jay", 0),
+    ("squat", "2025-08-09 00:00:00.000", "dev0-jay", 0),
+    ("plank", "2025-08-09 00:00:00.000", "dev0-jay", 1);
+
+INSERT INTO exercise_works_group (exercise_name, exercise_timestamp, created_by_user_id, muscle_group_name)
+VALUES ("bench-press", "2025-08-09 00:00:00.000", "dev0-jay", "chest"),
+    ("bench-press", "2025-08-09 00:00:00.000", "dev0-jay", "triceps"),
+    ("squat", "2025-08-09 00:00:00.000", "dev0-jay", "quads"),
+    ("squat", "2025-08-09 00:00:00.000", "dev0-jay", "glutes"),
+    ("plank", "2025-08-09 00:00:00.000", "dev0-jay", "core");
---
+++ exampledb/views.sql
@@ -0,0 +1,29 @@
+-- user view
+SELECT u.user_id, name, user_name_id
+FROM users u LEFT JOIN user_names un ON u.user_id = un.user_id
+WHERE NOT EXISTS (SELECT *
+    FROM user_names
+    WHERE user_names.prior_user_name_id = un.user_name_id)
+    AND u.user_id = "dev0-jay";
+
+-- exercise view    
+SELECT e.exercise_name, e.is_isometric, ewg.muscle_group_name, en.exercise_note,
+        name AS created_by_user_name
+        FROM exercises e LEFT JOIN exercise_works_group ewg ON e.exercise_name = ewg.exercise_name AND
+            e.created_by_user_id = ewg.created_by_user_id
+            LEFT JOIN exercise_notes en ON e.exercise_name = en.exercise_name AND
+            e.created_by_user_id = en.created_by_user_id
+            LEFT JOIN (
+                SELECT u.user_id, name, user_name_id
+                FROM users u LEFT JOIN user_names un ON u.user_id = un.user_id
+                WHERE NOT EXISTS (SELECT *
+                    FROM user_names
+                    WHERE user_names.prior_user_name_id = un.user_name_id
+                )) UserSubquery ON UserSubquery.user_id = e.created_by_user_id
+        WHERE NOT EXISTS (
+            SELECT *
+            FROM exercise_deletions ed
+            WHERE ed.exercise_name = e.exercise_name
+                AND ed.created_by_user_id = e.created_by_user_id)
+            AND e.exercise_name = "bench-press"
+            AND e.created_by_user_id = "dev0-jay";
